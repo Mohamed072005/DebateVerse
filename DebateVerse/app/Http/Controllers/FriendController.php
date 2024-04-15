0ebe16801ca426740efd963b037cc53c12a37d31
Mohamed072005 <amin072005@gmail.com>
2024-04-15 19:11:29 +0100

    add the controller and the model and the migration of the Friend part

--- DebateVerse/app/Http/Controllers/FriendController.php
+++ DebateVerse/app/Http/Controllers/FriendController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Friend;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FriendController extends Controller
 {
@@ -15,5 +16,11 @@ class FriendController extends Controller
         if ($senderExist) {
             return redirect()->route('home')->with('errorProfile', 'You already send friend request');
         }
+
+        Friend::create([
+            'sender_id' => Auth::user()->id,
+            'receiver_id' => $user->id
+        ]);
+        return redirect()->route('home')->with('successResponse', 'Friend request sent Successfully');
     }
 }
